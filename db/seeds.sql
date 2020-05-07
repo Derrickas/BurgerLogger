2e69a883b7bbae976cfeaab1e434a37fb7b88193
Derrickas <Derrickas@outlook.com>
2020-05-07 17:41:02 -0500

    adds project

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (name, devoured) VALUES ('cheeseburger', false);
+INSERT INTO burger (name, devoured) VALUES ('hamburger' true);
+INSERT INTO burger (name, devoured) VALUES ('baconburger', true);
+
